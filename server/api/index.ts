f660ad846ed7c2e26e1cf1113add13d38f3e4aac
tezzjob <tezzjob.services@gmail.com>
2025-03-02 03:22:34 +0530

    fix deployment

---
+++ server/api/index.ts
@@ -0,0 +1,59 @@
+import express from "express";
+import cors from "cors";
+import logger from "../src/utils/logger";
+import httpStatus from "http-status";
+import routes from "../src/routes/index.routes";
+import { ApiError, errorConverter, errorHandler } from "../src/middlewares/errors";
+require("dotenv").config();
+
+const app = express();
+
+// Use CORS
+app.use(cors({ origin: "*" }));
+
+app.use(express.json({ limit: "50mb" }));
+
+app.use("/heath-check", (_req, res) => {
+  res.send("Status: Healthy");
+});
+
+app.use("/error", async () => {
+  throw new Error();
+});
+
+app.use("/api", routes);
+
+// send back a 404 error for any unknown api request
+app.use((_req, _res, next) => {
+  logger.error("Not found");
+  next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
+});
+
+app.use(errorConverter);
+
+app.use(errorHandler);
+
+const PORT = 8080
+
+app.listen(PORT, (): void => console.log(`Server is running on ${PORT}`));
+
+module.exports = app;
+
+// import app from "./app";
+// import { APP_PORT } from "./utils/config";
+// import logger from "./utils/logger";
+
+// const init = async () => {
+//   logger.info("starting server..");
+
+//   app.listen(APP_PORT, () => {
+//     logger.info(`server listening on ${APP_PORT}`);
+//   });
+// };
+
+// init();
+
+// process.on("uncaughtException", (err) => {
+//   logger.error("Uncaught Exception: " + err);
+//   logger.error(err.stack);
+// });
