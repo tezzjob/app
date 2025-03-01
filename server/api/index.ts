c1392bdcb9dbfde5c50bf37f7118738913625054
tezzjob <tezzjob.services@gmail.com>
2025-03-02 03:27:40 +0530

    fix vercel deployment

--- server/api/index.ts
+++ server/api/index.ts
@@ -13,6 +13,10 @@ app.use(cors({ origin: "*" }));
 
 app.use(express.json({ limit: "50mb" }));
 
+app.use("/", (_req, res) => {
+  res.send("Success");
+});
+
 app.use("/heath-check", (_req, res) => {
   res.send("Status: Healthy");
 });
