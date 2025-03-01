5579fb2abb287fdaeaf1a9a1a39e5a5684fcc819
tezzjob <tezzjob.services@gmail.com>
2025-03-02 02:17:46 +0530

    fix: deployment

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,18 @@
 import app from "./src/app";
-import { VercelRequest, VercelResponse } from "@vercel/node";
+import { APP_PORT } from "./src/utils/config";
+import logger from "./src/utils/logger";
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return app(req, res);
-}
+const init = async () => {
+  logger.info("starting server..");
+
+  app.listen(APP_PORT, () => {
+    logger.info(`server listening on ${APP_PORT}`);
+  });
+};
+
+init();
+
+process.on("uncaughtException", (err) => {
+  logger.error("Uncaught Exception: " + err);
+  logger.error(err.stack);
+});
