5deb3d4a1b55f534a329424da31133dce29e9a4e
tezzjob <tezzjob.services@gmail.com>
2025-03-02 10:51:27 +0530

    fix deployment

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,7 +1,9 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 import app from "../src/app";
+import serverlessExpress from "@codegenie/serverless-express";
+
+const server = serverlessExpress({ app });
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const serverlessExpress = require("@codegenie/serverless-express");
-  exports.handler = serverlessExpress({ app });
+  return server(req, res);
 }
