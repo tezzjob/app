7b7a93dc1687f049982389fb8f832eaec6d6b76f
tezzjob <tezzjob.services@gmail.com>
2025-03-02 10:28:58 +0530

    fix deployment

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,6 +1,11 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-import app from '../src/app';
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import app from "../src/app";
+import { createServer } from "http";
+import { parse } from "url";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  return app(req, res);
+  const server = createServer(app);
+  const parsedUrl = parse(req.url!, true);
+
+  server.emit("request", req, res);
 }
