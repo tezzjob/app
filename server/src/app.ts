7c9a9ab0164e6b0a7f7901dc90985ee00b8c0f68
tezzjob <tezzjob.services@gmail.com>
2025-03-09 16:01:51 +0530

    fix deployment

--- server/src/app.ts
+++ server/src/app.ts
@@ -31,7 +31,7 @@ app.use("/", (_req, _res) => {
 
 app.use(express.json({ limit: "50mb" }));
 
-app.get('/api/heath-check', (_req, res) => {
+app.use('/api/heath-check', (_req, res) => {
     res.send('Status: Healthy')
 })
 
