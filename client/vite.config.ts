ed5a65697febe4c3ec262e8189b43a991f8b64ec
tezzjob <tezzjob.services@gmail.com>
2025-03-21 00:14:06 +0530

    fix: vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,15 +2,6 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig(({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return {
+export default defineConfig({
     plugins: [react()],
-    base: "/",
-    build: {
-      outDir: path.resolve(__dirname, "../build/client"),
-      emptyOutDir: true,
-    },
-  };
 });
