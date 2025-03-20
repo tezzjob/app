15c06ac30d72e7198e5fcfa631d1c4d9c2fc346b
tezzjob <tezzjob.services@gmail.com>
2025-03-21 00:29:02 +0530

    fix reload issues

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,5 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
     plugins: [react()],
